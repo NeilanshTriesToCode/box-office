4b7f996f6bd7323ae3d4774be14d9cc19780e601
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-05-28 16:51:20 +0530

    added react router to access different pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is the homepage
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+
+      <Route>Error 404: Page not found</Route>
+    </Switch>
+  );
 }
 
 export default App;
