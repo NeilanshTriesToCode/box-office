67e688b5e923647efee5f2420871ccf137ffd79e
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-06-07 20:42:42 +0530

    added memo to ShowCard

--- src/components/show/ShowCard.js
+++ src/components/show/ShowCard.js
@@ -1,5 +1,5 @@
 // React Component to display shows being displayed on the app
-import React from 'react';
+import React, { memo } from 'react';
 import { Link } from 'react-router-dom';
 import { Star } from '../styled';
 
@@ -30,4 +30,4 @@ const ShowCard = ({ id, image, name, summary, onStarClick, isStarred }) => {
   );
 };
 
-export default ShowCard;
+export default memo(ShowCard);
